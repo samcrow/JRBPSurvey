f12ed9d3de7f675bf0a72f075ef401203401fd9a
Sam Crow <sp4.cc.eatpie.info@gmail.com>
2025-11-28 19:30:43 -0800

    Fixed crash when starting an upload from the background

--- app/src/main/java/org/samcrow/ridgesurvey/data/NetworkBroadcastReceiver.java
+++ app/src/main/java/org/samcrow/ridgesurvey/data/NetworkBroadcastReceiver.java
@@ -17,21 +17,38 @@
 
 package org.samcrow.ridgesurvey.data;
 
+import android.app.BackgroundServiceStartNotAllowedException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 /**
  * Receives notifications of network changes and starts an {@link UploadService}
  */
 public class NetworkBroadcastReceiver extends BroadcastReceiver {
+    private static final String TAG = "NetworkBroadcastReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)
                 || intent.getAction().equals(Intent.ACTION_TIME_TICK)) {
-            // Start the upload service
-            context.startService(new Intent(context, UploadService.class));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                try {
+                    startUploadService(context);
+                } catch (BackgroundServiceStartNotAllowedException e) {
+                    Log.w(TAG, "Running in background, failed to start upload");
+                }
+            } else {
+                startUploadService(context);
+            }
         }
+    }
+
+    private void startUploadService(@NonNull Context context) {
+        context.startService(new Intent(context, UploadService.class));
     }
 }
